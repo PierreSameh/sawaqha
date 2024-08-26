7596854f72936240878300d1972b22e14252ff7e
PierreSameh <captainpirzon@gmail.com>
2024-08-26 15:22:38 +0300

    #p migrate

--- app/Http/Controllers/Admin/OrdersController.php
+++ app/Http/Controllers/Admin/OrdersController.php
@@ -111,10 +111,10 @@ class OrdersController extends Controller
         else if ($order->status === 3) {
             $order->status = 4;
             if ($order->user->user_type == 1) {
-                $order->user->expected_profit = $order->user->expected_profit - ((float) $order->total_sell_price - (float) $order->sub_total);
+                $order->user->expected_profit = $order->user->expected_profit + ((float) $order->total_sell_price - (float) $order->sub_total);
                 $order->user->balance = $order->user->balance + ((float) $order->total_sell_price - (float) $order->sub_total);
                 $order->user->save();
-                $transaction = Transaction::create([
+                $transaction = Transaction::create([ 
                     "user_id" => $order->user->id,
                     "order_id" => $order->id,
                     "type" => 1,
